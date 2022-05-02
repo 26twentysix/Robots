168d2f237b5c8eabb36f53734742ffe7fed858aa
lil angel <cvndyperov@gmail.com>
2022-05-02 21:13:57 +0500

    add some test files

---
+++ src/main/java/com/lilangel/views/game/enums/ButtonClickEvents.java
@@ -0,0 +1,10 @@
+package com.lilangel.views.game.enums;
+
+import com.lilangel.utils.Pair;
+
+public enum ButtonClickEvents {
+    CLICK_ON_PAUSE_BUTTON,
+    CLICK_ON_PLAY_BUTTON,
+    CLICK_ON_GAME_SPEED_INCREASE,
+    CLICK_ON_GAME_SPEED_DECREASE;
+}
