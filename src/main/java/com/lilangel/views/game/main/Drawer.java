5af2956071643ca5aadbb09ed75051156873a538
lilangel <cvndyperov@gmail.com>
2022-05-03 13:02:49 +0500

    removed unused methods

--- src/main/java/com/lilangel/views/game/main/Drawer.java
+++ src/main/java/com/lilangel/views/game/main/Drawer.java
@@ -10,25 +10,13 @@ public class Drawer {
         for (int i = 0; i < state.length; i++) {
             for (int j = 0; j < state[i].length; j++) {
                 switch (state[i][j]) {
-                    case WALL -> drawWall(g,j * 16, i * 16);
-                    case EMPTY -> drawEmpty(g,j * 16, i * 16);
-                    case ROBOT -> drawRobot(g,j * 16, i * 16);
-                    case ENERGY -> drawEnergy(g,j * 16, i * 16);
+                    case WALL -> drawWall(g, j * 16, i * 16);
+                    case EMPTY -> drawEmpty(g, j * 16, i * 16);
+                    case ROBOT -> drawRobot(g, j * 16, i * 16);
+                    case ENERGY -> drawEnergy(g, j * 16, i * 16);
                 }
             }
         }
-    }
-
-    private static int round(double value) {
-        return (int) (value + 0.5);
-    }
-
-    private static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2) {
-        g.fillOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
-    }
-
-    private static void drawOval(Graphics g, int centerX, int centerY, int diam1, int diam2) {
-        g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
     }
 
     private void drawRobot(Graphics2D g, int x, int y) {
@@ -47,7 +35,7 @@ public class Drawer {
         g.setColor(Color.BLACK);
         g.drawRect(x, y, 16, 16);
         g.setColor(Color.GRAY);
-        g.fillRect(x+1, y+1, 15, 15);
+        g.fillRect(x + 1, y + 1, 15, 15);
 
     }
 
