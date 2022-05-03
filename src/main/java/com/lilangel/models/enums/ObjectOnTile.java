622c6eda4e489fd540566b1e35e17695524850c1
lilangel <cvndyperov@gmail.com>
2022-05-03 15:09:39 +0500

    add some tail states and little refactor drawer

--- src/main/java/com/lilangel/models/enums/ObjectOnTile.java
+++ src/main/java/com/lilangel/models/enums/ObjectOnTile.java
@@ -7,7 +7,12 @@ public enum ObjectOnTile {
     EMPTY,
     ENERGY,
     ROBOT,
-    WALL;
+    WALL,
+    ROBOT_HP_DEAD,
+    ROBOT_ENERGY_DEAD,
+    ROBOT_VERTICAL_TRAIL,
+    ROBOT_HORIZONTAL_TRAIL;
+
 
     @Override
     public String toString(){
@@ -16,6 +21,10 @@ public enum ObjectOnTile {
             case ENERGY -> "$";
             case WALL -> "#";
             case ROBOT -> "*";
+            case ROBOT_ENERGY_DEAD -> "o";
+            case ROBOT_HP_DEAD -> "x";
+            case ROBOT_VERTICAL_TRAIL -> "-";
+            case ROBOT_HORIZONTAL_TRAIL -> "|";
         };
     }
 }
--- src/main/java/com/lilangel/views/game/main/Drawer.java
+++ src/main/java/com/lilangel/views/game/main/Drawer.java
@@ -14,16 +14,43 @@ public class Drawer {
                     case EMPTY -> drawEmpty(g, j * 16, i * 16);
                     case ROBOT -> drawRobot(g, j * 16, i * 16);
                     case ENERGY -> drawEnergy(g, j * 16, i * 16);
+                    case ROBOT_HP_DEAD -> drawHpDeadRobot(g, j * 16, i * 16);
+                    case ROBOT_ENERGY_DEAD -> drawEnergyDeadRobot(g, j * 16, i * 16);
+                    case ROBOT_HORIZONTAL_TRAIL -> drawRobotHorizontalTrail(g, j * 16, i * 16);
+                    case ROBOT_VERTICAL_TRAIL -> drawRobotVerticalTrail(g, j * 16, i * 16);
                 }
             }
         }
     }
 
     private void drawRobot(Graphics2D g, int x, int y) {
-        g.setColor(Color.BLACK);
-        g.drawRect(x, y, 16, 16);
+        drawEmpty(g, x, y);
         g.setColor(Color.BLUE);
         g.fillRect(x + 2, y + 2, 13, 13);
+    }
+
+    private void drawHpDeadRobot(Graphics2D g, int x, int y) {
+        drawEmpty(g, x, y);
+        g.setColor(Color.RED);
+        g.fillRect(x + 2, y + 2, 13, 13);
+    }
+
+    private void drawEnergyDeadRobot(Graphics2D g, int x, int y) {
+        drawEmpty(g, x, y);
+        g.setColor(Color.CYAN);
+        g.fillRect(x + 2, y + 2, 13, 13);
+    }
+
+    private void drawRobotHorizontalTrail(Graphics2D g, int x, int y) {
+        drawEmpty(g, x, y);
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(x + 1, y + 6, 15, 6);
+    }
+
+    private void drawRobotVerticalTrail(Graphics2D g, int x, int y) {
+        drawEmpty(g, x, y);
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(x + 6, y + 1, 6, 15);
     }
 
     private void drawEmpty(Graphics2D g, int x, int y) {
@@ -32,16 +59,14 @@ public class Drawer {
     }
 
     private void drawWall(Graphics2D g, int x, int y) {
-        g.setColor(Color.BLACK);
-        g.drawRect(x, y, 16, 16);
+        drawEmpty(g, x, y);
         g.setColor(Color.GRAY);
         g.fillRect(x + 1, y + 1, 15, 15);
 
     }
 
     private void drawEnergy(Graphics2D g, int x, int y) {
-        g.setColor(Color.BLACK);
-        g.drawRect(x, y, 16, 16);
+        drawEmpty(g, x, y);
         g.setColor(Color.PINK);
         g.fillRect(x + 4, y + 4, 8, 8);
     }
